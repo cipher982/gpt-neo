8b6e59766815173857f90f105e39196972c1c24c
David Rose <david010@gmail.com>
2021-03-26 22:11:41 +0000

    update setup to download model from gcloud (faster)

--- setup.sh
+++ setup.sh
@@ -6,9 +6,12 @@ sudo apt-get install apt-transport-https ca-certificates gnupg
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
 
-
 # Download the wine dataset
- gsutil -m cp -r gs://wine-gpt/scraped_wines.tfrecord /content/gpt-neo/scraped_wines.tfrecord
+gsutil -m cp -r gs://wine-gpt/scraped_wines.tfrecord /content/gpt-neo/scraped_wines.tfrecord
+
+# Download the GPT3 model
+mkdir /content/model_store
+gsutil -m cp -r gs://wine-gpt/GPT3_XL /content/model_store/GPT3_XL
 
 # Download the pretrained model
-wget -m -np -c -U "eye02" -w 2 -R "index.html*" "https://the-eye.eu/public/AI/gptneo-release/GPT3_XL/"
+#wget -m -np -c -U "eye02" -w 2 -R "index.html*" "https://the-eye.eu/public/AI/gptneo-release/GPT3_XL/"
